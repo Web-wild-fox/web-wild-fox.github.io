df6e536b51fe869ca8ea8725e77a5306f027e02a
Александр <33718719+Web-wild-fox@users.noreply.github.com>
2018-02-23 15:36:45 +0400

    +description, send.php, about us hov
    Улучшен адаптив для мобильных устройств, заполнен хэад, добавлен php скрипт для отправки формы, раздел "О нас" - добавлен эффект при наведении.

---
+++ send.php
@@ -0,0 +1,19 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['email'])&&$_POST['email']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'vrotmnenogi.12@mail.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Заявка с сайта Web wild fox'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Почта: '.$_POST['email'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
